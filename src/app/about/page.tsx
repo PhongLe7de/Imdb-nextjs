0d0b8a68f81c2a79632cf64d834b00c5dadc820c
PhongLe7de <phongle260260@gmail.com>
2025-01-09 11:19:23 +0200

    create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='max-w-6xl mx-auto p-3 space-y-4'>
+        <h1 className='text-2xl font-medium text-amber-600'>About</h1>
+        <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu orci ultrices, aliquam enim convallis, consectetur nisi. Vivamus maximus urna nec eros sollicitudin, eget faucibus nibh finibus. Donec sodales elit ac est efficitur viverra. Aliquam viverra pretium orci, non lobortis felis elementum ac. Nunc faucibus leo mollis sollicitudin dictum. Donec et lorem sit amet nunc congue aliquam et ut enim. Suspendisse eu vestibulum mi. Nunc sit amet sem luctus, auctor erat fringilla, molestie est. Donec id scelerisque tellus. Curabitur dui velit, feugiat sed enim eu, luctus dictum justo. Donec ut congue urna. Nulla sit amet eleifend mi. Ut congue, ante sit amet aliquet vulputate, purus nisl venenatis velit, ut vulputate quam ligula et turpis. Quisque sit amet nisi ligula. Nunc dignissim est et tempor molestie.        </p>
+        <p>
+        Ut porttitor pharetra consequat. Vivamus at nulla quis lectus ultrices pharetra. Etiam a vehicula odio. Nullam nec condimentum tortor, id congue ipsum. Sed fringilla, purus in suscipit placerat, est sem vestibulum tellus, eget rhoncus enim ante ac nulla. Curabitur consectetur justo id ex ullamcorper pharetra. Donec pharetra cursus tellus, ut sodales nisl aliquam nec. Vestibulum imperdiet enim nulla, dictum posuere ipsum venenatis et.
+        </p>
+        <p>Nullam egestas eros non sem blandit, eget ultricies risus ornare. Pellentesque pretium efficitur mi, ut pharetra enim varius nec. Etiam molestie iaculis commodo. Vivamus a sem lacus. Nulla facilisi. Nullam sit amet ultricies dui. Curabitur ut urna eget risus bibendum interdum.</p>
+    </div>
+  )
+}
