d0a6ab4f9c778dd3c1e706b1dbf580ddeb7083a7
PhongLe7de <phongle260260@gmail.com>
2025-01-09 13:21:14 +0200

    add loading effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,29 +9,25 @@ interface IHomeProps {
 
 async function Home({ searchParams }: IHomeProps) {
   const genre = searchParams?.genre || "fetchTrending";
-  try {
-    const endpoint =
-      genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week";
+  const endpoint =
+    genre === "fetchTopRated" ? "/movie/top_rated" : "/trending/all/week";
 
-    const res = await fetch(
-      `https://api.themoviedb.org/3${endpoint}?api_key=${API_KEY}&language=en-US&page=1`
-    );
+  const res = await fetch(
+    `https://api.themoviedb.org/3${endpoint}?api_key=${API_KEY}&language=en-US&page=1`
+  );
 
-    if (!res.ok) {
-      throw new Error(`Failed to fetch data: ${res.statusText}`);
-    }
-
-    const data = await res.json();
-    const results = data.results as IMovie[]
-    
-    return (
-      <div>
-        <Results results={results} />
-      </div>
-    );
-  } catch (error) {
-    return <div>Failed to load movies. Please try again later.</div>;
+  if (!res.ok) {
+    throw new Error(`Failed to fetch data: ${res.statusText}`);
   }
+
+  const data = await res.json();
+  const results = data.results as IMovie[];
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }
 
 export default Home;
